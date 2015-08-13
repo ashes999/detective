606da1cc1bc33ff5e808b5793702233e4250d338
ashes999 <ashes999@yahoo.com>
2015-08-13 15:23:06 -0400

    Slight refactoring; delete comments. For #1

--- Scripts/profiles_scene.rb
+++ Scripts/profiles_scene.rb
@@ -1,110 +1,73 @@
 class Scene_Profiles < Scene_ItemBase
-  #--------------------------------------------------------------------------
-  # * Start Processing
-  #--------------------------------------------------------------------------
+  # Start Processing
   def start
     super
     create_suspects_window
     create_details_window
   end
-  #--------------------------------------------------------------------------
-  # * Create Category Window
-  #--------------------------------------------------------------------------
+
   def create_suspects_window
     @suspect_list = Window_SuspectsList.new
     @suspect_list.viewport = @viewport
-    @suspect_list.set_handler(:ok,     method(:on_category_ok))
+    @suspect_list.set_handler(:ok,     method(:on_suspect_ok))
     @suspect_list.set_handler(:cancel, method(:return_scene))
   end
-  #--------------------------------------------------------------------------
-  # * Create Item Window
-  #--------------------------------------------------------------------------
+
   def create_details_window
     wy = @suspect_list.y + @suspect_list.height
     wh = Graphics.height - wy
     @details_window = Window_ItemList.new(0, wy, Graphics.width, wh)
     @details_window.viewport = @viewport
-    @details_window.set_handler(:ok,     method(:on_item_ok))
-    @details_window.set_handler(:cancel, method(:on_item_cancel))
+    #@details_window.set_handler(:ok,     method(:on_item_ok))
+    #@details_window.set_handler(:cancel, method(:on_item_cancel))
     @suspect_list.details_window = @details_window
   end
-  #--------------------------------------------------------------------------
-  # * Category [OK]
-  #--------------------------------------------------------------------------
-  def on_category_ok
+
+  # when you select a category (suspect)
+  def on_suspect_ok
     @details_window.activate
     @details_window.select_last
   end
-  #--------------------------------------------------------------------------
-  # * Item [OK]
-  #--------------------------------------------------------------------------
-  def on_item_ok
-    $game_party.last_item.object = item
-    determine_item
-  end
-  #--------------------------------------------------------------------------
-  # * Item [Cancel]
-  #--------------------------------------------------------------------------
-  def on_item_cancel
-    @details_window.unselect
-    @suspect_list.activate
-  end
-  #--------------------------------------------------------------------------
-  # * Play SE When Using Item
-  #--------------------------------------------------------------------------
-  def play_se_for_item
-    Sound.play_use_item
-  end
-  #--------------------------------------------------------------------------
-  # * Use Item
-  #--------------------------------------------------------------------------
-  def use_item
-    super
-    @suspect_list.redraw_current_item
-  end
+
+  #def on_item_ok
+  #  $game_party.last_item.object = item
+  #  determine_item
+  #end
+
+  #def on_item_cancel
+  #  @details_window.unselect
+  #  @suspect_list.activate
+  #end
 end
 
 
 class Window_SuspectsList < Window_HorzCommand
-  #--------------------------------------------------------------------------
-  # * Public Instance Variables
-  #--------------------------------------------------------------------------
   attr_reader   :suspect_list
-  #--------------------------------------------------------------------------
-  # * Object Initialization
-  #--------------------------------------------------------------------------
+
   def initialize
     super(0, 0)
   end
-  #--------------------------------------------------------------------------
-  # * Get Window Width
-  #--------------------------------------------------------------------------
+
   def window_width
     Graphics.width
   end
   
-  ### Maximum number of items to show at one item. Items are fixed width :(
+  ### Maximum number of items to show at one time. Items are fixed width :(
   def col_max
     return 6
   end
-  #--------------------------------------------------------------------------
-  # * Frame Update
-  #--------------------------------------------------------------------------
+
   def update
     super
     @suspect_list.category = current_symbol if @suspect_list
   end
-  #--------------------------------------------------------------------------
-  # * Create Command List
-  #--------------------------------------------------------------------------
+
   def make_command_list
     DetectiveGame::instance.npcs.each do |n|
       add_command(n.name, n.name.to_sym)
     end
   end
-  #--------------------------------------------------------------------------
-  # * Set Item Window
-  #--------------------------------------------------------------------------
+
   def details_window=(details_window)
     @details_window = details_window
     update

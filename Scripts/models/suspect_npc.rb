519b84f5e8680a4dbe538793412ad24b2a229f7e
ashes999 <ashes999@yahoo.com>
2015-09-01 18:17:48 -0400

    Added criminal records to profile.

--- Scripts/models/suspect_npc.rb
+++ Scripts/models/suspect_npc.rb
@@ -8,6 +8,9 @@ class SuspectNpc < Npc
   
   NPC_PROFESSIONS = ['janitor', 'programmer', 'accountant', 'business analyst', 'personal trainer', 'CEO', 'teacher', 'cop', 'journalist']
   BLOOD_TYPES = ['A', 'B', 'AB', 'O']
+  NEGLIBLE_CRIMES = ['petty theft', 'shoplifting', 'vandalism', 'breaking and entering']
+  MINOR_CRIMES = ['extortion', 'assault', 'robbery', 'embezzlement', 'substance abuse']
+  MAJOR_CRIMES = ['armed robbery', 'homicide', 'kidnapping', 'arson', 'child abuse']
   
   # move_speed = 1-6
   # move_frequency = 1-5
@@ -21,7 +24,8 @@ class SuspectNpc < Npc
     # basic facts
     @age = 20 + rand(15)    
     @profession = NPC_PROFESSIONS.sample
-    @blood_type = pick_blood_type    
+    @blood_type = pick_blood_type
+    @criminal_record = generate_criminal_record
   end
  
   def talk    
@@ -35,18 +39,28 @@ class SuspectNpc < Npc
   end
   
   def profile
-    return "#{@name} is a #{@age} year-old #{@profession} with blood type #{@blood_type}."
+    return "#{@name} is a #{@age} year-old #{@profession} with blood type #{@blood_type}.\n#{@criminal_record}"
   end
   
   private
   
   def pick_blood_type
     # Based on culmulative distribution from http://www.redcrossblood.org/learn-about-blood/blood-types
-    # O: 48% A: 31% B: 16% AB: 4%    
+    # O: 48% A: 31% B: 16% AB: 4%
+    # i.e. 50% O, 80% O and A
     blood_picked = rand(100)
     return 'O' if blood_picked < 48
     return 'A' if blood_picked < 48 + 31
     return 'B' if blood_picked < 48 + 31 + 16
     return 'AB'
+  end
+  
+  def generate_criminal_record
+    severity = rand(100)
+    # 30% nothing, 30% mild, 25% medium, 15% severe
+    return "#{@name} has no prior criminal record" if severity < 30
+    return "#{@name}'s criminal record contains a few counts of #{NEGLIBLE_CRIMES.sample}" if severity < 60
+    return "#{@name} served a short jail sentence for #{MINOR_CRIMES.sample}" if severity < 85
+    return "#{@name} served several years of combined jail time for #{MAJOR_CRIMES.sample(2).join(' and ')}" # >= 85
   end
 end
